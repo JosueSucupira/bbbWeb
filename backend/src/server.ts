a0ac20d4ae99624a72f1f07026c51f832441325b
João Victor <joaocarvoli@hotmail.com>
2023-09-19 23:43:33 -0300

    Creating backend

---
+++ backend/src/server.ts
@@ -0,0 +1,17 @@
+import express, { Express, Request, Response } from 'express';
+import cors from 'cors'
+import options from './cors'
+
+const app: Express = express();
+const SERVER_PORT = 8000
+
+app.use(cors(options))
+app.use(express.json())
+
+app.get('/', (req: Request, res: Response) => {
+  res.json({ message: "Hello from server!" });
+});
+
+app.listen(SERVER_PORT, () => {
+  console.log(`[server]: Server is running at http://localhost:${SERVER_PORT}`);
+});
